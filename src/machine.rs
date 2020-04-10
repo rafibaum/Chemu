bfa2c4a7ca957f3425131668c978fafcc31f12e5
Rafi Baum <rafi@ukbaums.com>
2020-04-09 23:15:15 -0400

    Implemented machine initialisation

--- src/machine.rs
+++ src/machine.rs
@@ -2,6 +2,69 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt;
 use std::fmt::{Display, Formatter};
+use std::fs::File;
+use std::io::Read;
+
+const DIGITS: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+    0x20, 0x60, 0x20, 0x20, 0x70, // 1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+    0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+    0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+    0xF0, 0x80, 0xF0, 0x80, 0x80, // F
+];
+
+const PROGRAM_START: usize = 512;
+const MEMORY_SIZE: usize = 4096;
+const STACK_START: usize = DIGITS.len();
+
+pub struct Machine {
+    registers: Vec<u8>,
+    address_register: u16,
+    program_counter: u16,
+    stack_pointer: u16,
+    delay_timer: u8,
+    sound_timer: u8,
+    memory: Vec<u8>,
+}
+
+impl Machine {
+    pub fn from_file(file: &mut File) -> Result<Machine, std::io::Error> {
+        let mut memory = Vec::with_capacity(MEMORY_SIZE);
+
+        // Copy program data into memory
+        if let Err(e) = file.read_exact(&mut memory[PROGRAM_START..]) {
+            if e.kind() != std::io::ErrorKind::UnexpectedEof {
+                return Err(e);
+            }
+        };
+
+        // Copy digit layouts into memory
+        for (i, byte) in DIGITS.iter().enumerate() {
+            memory[i] = *byte;
+        }
+
+        Ok(Machine {
+            registers: Vec::new(),
+            address_register: 0,
+            program_counter: PROGRAM_START as u16,
+            stack_pointer: STACK_START as u16,
+            delay_timer: 0,
+            sound_timer: 0,
+            memory,
+        })
+    }
+}
 
 /// Represents all the registers directly available to programs in the Chip-8 architecture. Each
 /// stores a byte of information.
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use crate::machine::Machine;
 use std::fs::File;
-use std::io::{BufReader, Read};
 
 mod instruction;
 mod machine;
@@ -16,7 +16,7 @@ fn main() {
         }
     };
 
-    let file = match File::open(file_path) {
+    let mut file = match File::open(file_path) {
         Ok(file) => file,
         Err(e) => {
             eprintln!("Could not open file");
@@ -25,38 +25,12 @@ fn main() {
         }
     };
 
-    let mut reader = BufReader::new(file);
-
-    let mut buffer = [0; 2];
-    let mut bytes_read = 0;
-    let mut output = Vec::new();
-
-    'reading: loop {
-        let result = reader.read_exact(&mut buffer);
-        if let Err(e) = result {
-            if e.kind() == std::io::ErrorKind::UnexpectedEof {
-                break 'reading;
-            } else {
-                eprintln!("An IO error has occurred");
-                eprintln!("Cause: {}", e);
-                return;
-            }
+    let _machine = match Machine::from_file(&mut file) {
+        Ok(machine) => machine,
+        Err(e) => {
+            eprintln!("Couldn't read file");
+            eprintln!("Cause: {}", e);
+            return;
         }
-
-        let instruction = instruction::decode(u16::from_be_bytes(buffer));
-        match instruction {
-            Ok(decoded) => output.push(decoded),
-            Err(e) => {
-                eprintln!("Instruction decoding error at: 0x{:X}", bytes_read);
-                eprintln!("Cause: {}", e);
-                return;
-            }
-        }
-
-        bytes_read += 2;
-    }
-
-    for (i, instruction) in output.iter().enumerate() {
-        println!("{}: {:?}", 512 + i * 2, instruction);
-    }
+    };
 }
